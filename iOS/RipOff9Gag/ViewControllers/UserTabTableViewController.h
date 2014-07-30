84a90ffa7b9e5a968725128d0e3796a899c930d0
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-07-31 00:20:52 +0800

    added registration form

---
+++ iOS/RipOff9Gag/ViewControllers/UserTabTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserTabTableViewController.h
+//  RipOff9Gag
+//
+//  Created by Lyndon Michael Bibera on 7/30/14.
+//  Copyright (c) 2014 Secret Transaction Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserTabTableViewController : UITableViewController
+
+@end
