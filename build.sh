1cf9048c5d662a69de621839af8d533d1eb1c523
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-08-05 21:29:50 +0800

    improved shell script for building libraries

--- build.sh
+++ build.sh
@@ -1,19 +1,23 @@
 echo "Starting Script..."
 echo "Generating Discovery Files..."
 
-endpointscfg.py get_discovery_doc --format rpc --application server/ripoff9gag --hostname "localhost:8080" --output endpoints api.ReactionAPI api.PostsAPI api.UserAPI
+domain="localhost:8080"
+service_generator="/Users/lbibera/Library/Developer/Xcode/DerivedData/ServiceGenerator-auogvovcdkjnxnblqocbhybotoxv/Build/Products/Debug/ServiceGenerator"
+output_dir="endpoints"
+
+endpointscfg.py get_discovery_doc --format rpc --application server/ripoff9gag --hostname "$domain" --output endpoints api.ReactionAPI api.PostsAPI api.UserAPI
 
 echo "Generating iOS Library..."
 
-/Users/lbibera/Library/Developer/Xcode/DerivedData/ServiceGenerator-auogvovcdkjnxnblqocbhybotoxv/Build/Products/Debug/ServiceGenerator \
+$service_generator \
     endpoints/post-v1.discovery \
-    --outputDir endpoints/API/Post    
-/Users/lbibera/Library/Developer/Xcode/DerivedData/ServiceGenerator-auogvovcdkjnxnblqocbhybotoxv/Build/Products/Debug/ServiceGenerator \
+    --outputDir $output_dir/API/Post    
+$service_generator  \
     endpoints/reaction-v1.discovery \
-    --outputDir endpoints/API/Reaction  
-/Users/lbibera/Library/Developer/Xcode/DerivedData/ServiceGenerator-auogvovcdkjnxnblqocbhybotoxv/Build/Products/Debug/ServiceGenerator \
+    --outputDir $output_dir/API/Reaction  
+$service_generator \
     endpoints/user-v1.discovery \
-    --outputDir endpoints/API/User
+    --outputDir $output_dir/API/User
 
 echo "Deleting Useless Files..."
 
