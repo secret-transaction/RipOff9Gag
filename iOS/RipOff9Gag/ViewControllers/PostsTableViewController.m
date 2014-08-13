2a00c7f79a2c3eca761d93cd58b6b0294ab04335
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-08-14 01:33:37 +0800

    added simple remote query

--- iOS/RipOff9Gag/ViewControllers/PostsTableViewController.m
+++ iOS/RipOff9Gag/ViewControllers/PostsTableViewController.m
@@ -43,6 +43,21 @@
 - (void)refreshTable
 {
     NSLog(@"Refresh");
+    
+    GTLServicePost *service = [GTLServicePost new];
+    
+    GTLQueryPost *query = [GTLQueryPost queryForListWithType:kGTLPostTypeHot];
+    query.cursor = @"";
+    query.pageSize = 10L;
+    
+    [service executeQuery:query completionHandler:^(GTLServiceTicket *ticket, GTLPostApiPostsUserPostListResponse *response, NSError *error) {
+        if (!error) {
+            NSLog(@"Listing Posts Success");
+        } else {
+            NSLog(@"Login Failed:%@", error);
+        }
+    }];
+
 }
 
 #pragma mark - Table view data source
@@ -64,6 +79,7 @@
     
     UserPost *userPost = [self.fetchedResultsController objectAtIndexPath:indexPath];
     cell.title.text = userPost.title;
+    
     return cell;
 }
 
@@ -91,54 +107,5 @@
     
     return _fetchedResultsController;
 }
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
