fe193923b61592f5eadda397f995cf6a4d4f4a04
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-08-19 14:42:51 +0800

    added initial java GAE project

---
+++ server-java/src/main/java/com/secrettransaction/rogagserver/YourFirstAPI.java
@@ -0,0 +1,8 @@
+package com.secrettransaction.rogagserver;
+
+/**
+  * Add your first API methods in this class, or you may create another class. In that case, please
+  * update your web.xml accordingly.
+ **/
+public class YourFirstAPI {
+}
